625c005e1547624a24db326001d08e93ce435d5c
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-04-23 20:57:27 +0200

    fix

--- rt.c
+++ rt.c
@@ -1,13 +1,15 @@
-#include "rt.h"
+#include "rt/rt.h"
+ 
+#if USE_GLAMOUR
 
-#ifdef USE_GLAMOUR
-#include "src.h"
+#include "rt/src.h"
 
 char *md_render(const char *src, size_t wrap) {
     return MdRender((char*) src, wrap);
 }
 #else
 #include <string.h>
+#warning not using glamour!
 
 char *md_render(const char *src, size_t wrap) {
     (void) wrap;
--- rt/build.sh
+++ rt/build.sh
@@ -1,16 +1,17 @@
 set -e
+set -o xtrace
 
 if [ "$1" == "glamour" ]; then
     echo "glamour markdown renderer"
     go build -buildmode=c-archive
     patch -u < fixsrch.patch 1>/dev/null 2>/dev/null || true
-    $CC -DUSE_GLAMOUR $CFLAGS -c rt.c
+    $CC -DUSE_GLAMOUR=1 $CFLAGS -c ../rt.c -o rt.o
     cp src.a rt.a
     ar -r rt.a rt.o
     echo "generated rt.a"
 elif [ "$1" == "txt" ]; then
     echo "no markdown renderer"
-    tcc -c rt.c
+    $CC $CFLAGS -c ../rt.c -o rt.o
     ar -rs rt.a rt.o
     echo "generated rt.a"
 else
